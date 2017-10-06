9aaf2c68fe03c2fa4d4597eb4fd5923c4332b91d
juju4 <juju4@users.noreply.github.com>
2017-10-05 22:45:48 -0400

    review travis tests

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,11 @@ rolesdir=$(dirname $0)/..
 fi
 
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/geerlingguy.apache ] && git clone https://github.com/geerlingguy/ansible-role-apache $rolesdir/geerlingguy.apache
+[ ! -d $rolesdir/geerlingguy.nginx ] && git clone https://github.com/geerlingguy/ansible-role-nginx $rolesdir/geerlingguy.nginx
+[ ! -d $rolesdir/juju4.harden-apache ] && git clone https://github.com/juju4/ansible-harden-apache $rolesdir/juju4.harden-apache
+[ ! -d $rolesdir/juju4.harden-nginx ] && git clone https://github.com/juju4/ansible-harden-nginx $rolesdir/juju4.harden-nginx
+
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.up1 ] && ln -s ansible-up1 $rolesdir/juju4.up1
 [ ! -e $rolesdir/juju4.up1 ] && cp -R $rolesdir/ansible-up1 $rolesdir/juju4.up1
