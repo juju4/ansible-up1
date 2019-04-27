f532a14c8ae78f9ccc94cafa32d2e3c3cd969139
juju4 <juju4@users.noreply.github.com>
2019-04-27 09:31:46 -0400

    test/default: add serverspec tests for npm list+outdated - chdir

--- test/integration/default/serverspec/up1_spec.rb
+++ test/integration/default/serverspec/up1_spec.rb
@@ -48,18 +48,18 @@ describe file("#{up1_working_dir}/client/config.js") do
   it { should be_owned_by "root" }
 end
 
-describe command("sudo -u www-up1 -H npm list") do
+describe command("cd #{up1_working_dir}/server && sudo -u www-up1 -H npm list") do
 #  let(:sudo_options) { '-u www-up1 -H' }
-  let(:pre_command) { "cd #{up1_working_dir}/server" }
+#  let(:pre_command) { "cd #{up1_working_dir}/server" }
   its(:stdout) { should match /busboy/ }
   its(:stdout) { should match /request/ }
   its(:stderr) { should match "" }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("sudo -u www-up1 -H npm outdated") do
+describe command("cd #{up1_working_dir}/server && sudo -u www-up1 -H npm outdated") do
 #  let(:sudo_options) { '-u www-up1 -H' }
-  let(:pre_command) { "cd #{up1_working_dir}/server" }
+#  let(:pre_command) { "cd #{up1_working_dir}/server" }
   its(:stdout) { should match "" }
   its(:stderr) { should match "" }
   its(:exit_status) { should eq 0 }
